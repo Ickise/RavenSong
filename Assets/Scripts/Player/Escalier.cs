b67edeac86ced2c3660bf67ec4716faf0abbb1f6
Neroxgd <darkaux69@gmail.com>
2023-11-15 16:45:05 +0100

    escalier v1

--- Assets/Scripts/Player/Escalier.cs
+++ Assets/Scripts/Player/Escalier.cs
@@ -1,6 +1,17 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Escalier : MonoBehaviour
 {
-    
+    [SerializeField] private Transform plateforme, downPoint, upPoint;
+
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            plateforme.position = new Vector2(transform.position.x, Mathf.Lerp(downPoint.position.y, upPoint.position.y, 1f - ((upPoint.position.x - other.transform.position.x) / (upPoint.position.x - downPoint.position.x))));
+            print((upPoint.position.x - other.transform.position.x) / (upPoint.position.x - downPoint.position.x));
+        }
+    }
 }
