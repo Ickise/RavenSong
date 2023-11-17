538f86b8a1b7730efbe234028aa8c364d824d266
Neroxgd <darkaux69@gmail.com>
2023-11-17 11:58:55 +0100

    plateforme

---
+++ Assets/Scripts/Player/Plateforme.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Plateforme : MonoBehaviour
+{
+    [SerializeField] private GameObject plateformeCollider;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        plateformeCollider.SetActive(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        plateformeCollider.SetActive(false);
+    }
+
+    public void PassDown(InputAction.CallbackContext context)
+    {
+        if (context.ReadValue<Vector2>().y < 0)
+            plateformeCollider.SetActive(false);
+    }
+}
