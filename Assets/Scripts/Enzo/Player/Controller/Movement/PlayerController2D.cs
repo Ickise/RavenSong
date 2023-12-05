95aa63bf87d017fd090a4f4c9a9d87f59c80f24b
Ickise <enzo.dufour05@gmail.com>
2023-12-05 15:47:36 +0100

    Modif préfab

--- Assets/Scripts/Enzo/Player/Controller/Movement/PlayerController2D.cs
+++ Assets/Scripts/Enzo/Player/Controller/Movement/PlayerController2D.cs
@@ -4,17 +4,39 @@ public class PlayerController2D : MonoBehaviour
 {
     [Header("À set up")]
     [SerializeField] private float speed = 5f;
-    [SerializeField] private float gravity = 1f;
-
+    [SerializeField] private float gravityFactor = 1f;
+    [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private float hangTime = 0.1f;
+    [SerializeField] private float jumpBufferLength = 0.1f; 
+    [SerializeField] private float fallMultiplier = 2.5f;
+    [SerializeField] private float lowJumpMultiplier = 2f;
+    
     [SerializeField] private Rigidbody2D playerRigidbody2D;
 
     [SerializeField] private RaycastDetection _raycastDetection;
+
+    [Header("Ne pas set up")]
+    [SerializeField] private float hangTimeCounter; 
+    [SerializeField] private float jumpBufferCounter; 
     
-    [SerializeField] private AudioClip runAudio;
-    
+    private bool canJump => jumpBufferCounter > 0 && hangTimeCounter > 0;
+
+    private Vector2 playerVelocity;
+
+    private void Update()
+    {
+        CoyoteTime();
+        JumpBuffer();
+    }
+
     private void FixedUpdate()
     {
         ModularMovement();
+        SetGravity();
+        ComputeGravity();
+        if (canJump) Jump();
+        
+        playerRigidbody2D.velocity = playerVelocity;
     }
 
     private void ModularMovement()
@@ -22,20 +44,57 @@ public class PlayerController2D : MonoBehaviour
         if (InputReader.instance.direction.x > 0 && !_raycastDetection.stopRight ||
             InputReader.instance.direction.x < 0 && !_raycastDetection.stopLeft)
         {
-            playerRigidbody2D.velocity = new Vector2(InputReader.instance.direction.x * speed, playerRigidbody2D.velocity.y);
+            playerVelocity.x = InputReader.instance.direction.x * speed;
         }
         else
         {
-            playerRigidbody2D.velocity = new Vector2(0, playerRigidbody2D.velocity.y);
+            playerVelocity.x = 0;
         }
-      
+    }
+
+    private void SetGravity()
+    {
         if (_raycastDetection.isGrounded)
         {
-            playerRigidbody2D.gravityScale = 0;
+            playerVelocity.y = 0;
         }
         else
         {
-            playerRigidbody2D.gravityScale = gravity;
+            playerVelocity.y += Physics2D.gravity.y * Time.deltaTime * gravityFactor;
         }
+    }
+    
+    private void Jump()
+    {
+        hangTimeCounter = 0f;
+        jumpBufferCounter = 0f;
+        
+        playerVelocity.y = jumpForce;
+    }
+
+    private void ComputeGravity()
+    {
+        bool isFalling = playerVelocity.y < 0;
+        bool isReleasingJump = playerVelocity.y > 0 && !InputReader.instance.jump;
+        
+        if (!isFalling && !isReleasingJump)
+        {
+            return;
+        }
+
+        float factor = isFalling ? fallMultiplier : lowJumpMultiplier;
+        playerVelocity += Vector2.up * Physics2D.gravity.y * (factor - 1) * Time.deltaTime;
+    }
+
+    private void CoyoteTime()
+    {
+        if (_raycastDetection.isGrounded) hangTimeCounter = hangTime;
+        else hangTimeCounter -= Time.deltaTime;
+    }
+
+    private void JumpBuffer()
+    {
+        if (InputReader.instance.jump) jumpBufferCounter = jumpBufferLength;
+        else jumpBufferCounter -= Time.deltaTime;
     }
 }
