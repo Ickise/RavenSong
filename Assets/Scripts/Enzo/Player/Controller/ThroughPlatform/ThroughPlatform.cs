a049b1df08cda3b7b5ead4dccf37b88d10966e48
Ickise <enzo.dufour05@gmail.com>
2023-12-07 16:42:08 +0100

    Plateforme fonctionnelle, juste voir avec les GD ce qu'ils en pensent et faire un préfab

--- Assets/Scripts/Enzo/Player/Controller/ThroughPlatform/ThroughPlatform.cs
+++ Assets/Scripts/Enzo/Player/Controller/ThroughPlatform/ThroughPlatform.cs
@@ -2,35 +2,22 @@ using UnityEngine;
 
 public class ThroughPlatform : MonoBehaviour
 {
-    [SerializeField] private bool isUp;
-
-    [SerializeField] private GameObject player;
-
-    //  [SerializeField] private RaycastDetection _raycastDetection;
-    // mettre dans une plateforme un collider, mettre deux gameobjects avec chacun un collider, il y en a un au dessus et l'autre en dessous (up/down) et activer le bool up pour celui du dessus.
-// trouver un moyen de bloquer le jump direct après la plateforme.
+    [Header("À set up")]
+    [SerializeField] private RaycastDetection _raycastDetection;
+    
     private void Update()
     {
-        if (InputReader.instance.canDown && player.GetComponentInChildren<RaycastDetection>().isGrounded)
+        if (InputReader.instance.canDown && _raycastDetection.isGrounded)
         {
-//            player.GetComponentInChildren<RaycastDetection>().isGrounded ou _raycastDetection.isGrounded
             transform.parent.GetComponent<Collider2D>().enabled = false;
         }
     }
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            transform.parent.GetComponent<Collider2D>().enabled = isUp;
-        }
-    }
-
+    
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            transform.parent.GetComponent<Collider2D>().enabled = false;
+            transform.parent.GetComponent<Collider2D>().enabled = true;
         }
     }
 }
