766db921c3227db8fc7b69b993236c40eebc29c8
Ickise <enzo.dufour05@gmail.com>
2023-12-14 12:12:02 +0100

    test capsule cast

--- Assets/Scripts/Enzo/Raycast/Player/RaycastDetection.cs
+++ Assets/Scripts/Enzo/Raycast/Player/RaycastDetection.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 public class RaycastDetection : MonoBehaviour
@@ -40,10 +39,8 @@ public class RaycastDetection : MonoBehaviour
         capsuleCastLeftHit = Physics2D.CapsuleCast(capsuleCastLeft.position, capsuleSizeWall, CapsuleDirection2D.Vertical,
             capsuleAngle, Vector2.right, distanceToDetectWall, layerWall);
         
-       // capsuleCastGroundHit = Physics2D.CapsuleCast(capsuleCastGround.position, capsuleSizeGround, CapsuleDirection2D.Horizontal,
-         //   capsuleAngle, Vector2.down, distanceToDetectFloor, layerGround);
-
-        capsuleCastGroundHit = Physics2D.BoxCast(capsuleCastGround.position, capsuleSizeGround, capsuleAngle, Vector2.down, distanceToDetectFloor, layerGround);
+        capsuleCastGroundHit = Physics2D.CapsuleCast(capsuleCastGround.position, capsuleSizeGround, CapsuleDirection2D.Horizontal,
+            capsuleAngle, Vector2.down, distanceToDetectFloor, layerGround);
     }
 
     private void SetBool()
@@ -52,11 +49,5 @@ public class RaycastDetection : MonoBehaviour
 
         stopLeft = capsuleCastLeftHit;
         stopRight = capsuleCastRightHit;
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawCube(capsuleCastGround.position, capsuleSizeGround);
     }
 }
