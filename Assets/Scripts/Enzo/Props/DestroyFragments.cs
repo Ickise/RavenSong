4fef18394bd0c4509e386d7c8904a4c541687865
Ickise <enzo.dufour05@gmail.com>
2024-01-30 15:10:04 +0100

    Les fragments se détruisent

--- Assets/Scripts/Enzo/Props/DestroyFragments.cs
+++ Assets/Scripts/Enzo/Props/DestroyFragments.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
 public class DestroyFragments : MonoBehaviour
 {
-  private void Start()
-  {
-    Destroy(gameObject, 5f);
-  }
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
 }
