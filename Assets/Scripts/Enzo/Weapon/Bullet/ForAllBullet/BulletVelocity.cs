f3ae2b0de2e3e30c0a25ea26a055dfa5116aeca0
Ickise <enzo.dufour05@gmail.com>
2024-02-08 12:23:51 +0100

    Animation chandelier

--- Assets/Scripts/Enzo/Weapon/Bullet/ForAllBullet/BulletVelocity.cs
+++ Assets/Scripts/Enzo/Weapon/Bullet/ForAllBullet/BulletVelocity.cs
@@ -34,7 +34,7 @@ public class BulletVelocity : MonoBehaviour
     {
         //  AudioManager.instance.PlaySFX(impactAudio);
 
-        baseBulletRigidbody2D.velocity = _bulletDirection.direction.normalized * xSpeedBullet;
+        baseBulletRigidbody2D.velocity = _bulletDirection.direction * xSpeedBullet;
 
         if (_bulletCollisionDetection.hitGround || _bulletCollisionDetection.hitSomething ||
             InputReader.instance.canRecall)
