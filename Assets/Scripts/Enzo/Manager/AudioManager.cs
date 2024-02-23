0e1119a8ecb04c1c6c5ab6d910b52175aa289cd0
Ickise <enzo.dufour05@gmail.com>
2024-02-23 15:16:02 +0100

    Update AudioManager.cs
    J'ai ajouté une méthode pour jouer un son random parmi une liste de son

--- Assets/Scripts/Enzo/Manager/AudioManager.cs
+++ Assets/Scripts/Enzo/Manager/AudioManager.cs
@@ -20,4 +20,11 @@ public class AudioManager : MonoBehaviour
         mainAudioSource.outputAudioMixerGroup = data.AudioMixerGroup;
         mainAudioSource.PlayOneShot(data.AudioToPlay);
     }
+    
+    public void PlayRandomSound(SoundData[] listOfSoundData)
+    {
+        SoundData randomClip = listOfSoundData[Random.Range(0, listOfSoundData.Length)];
+
+        PlaySFX(randomClip);
+    }
 }
