40837768a9c73f89e6e9b6bc1fa92e3a95f22d2d
Ickise <enzo.dufour05@gmail.com>
2024-03-12 11:42:17 +0100

    Modification BulletDirection
    Normalement, on a la direction de la manette qui fonctionne.

--- Assets/Scripts/Enzo/Weapon/Bullet/ForAllBullet/BulletDirection.cs
+++ Assets/Scripts/Enzo/Weapon/Bullet/ForAllBullet/BulletDirection.cs
@@ -10,23 +10,16 @@ public class BulletDirection : MonoBehaviour
         Init();
     }
 
-    public void Init()
+    private void Init()
     {
         Camera mainCamera = Camera.main;
-
-        Transform rightAimPosition =
-            PlayerController2D._instance.GetComponentInChildren<FireOneBullet>().rightAimPosition;
-
-        Transform leftAimPosition =
-            PlayerController2D._instance.GetComponentInChildren<FireOneBullet>().leftAimPosition;
-
-        Vector3 currentDirection = PlayerController2D._instance.CurrentDirectionAim > 0
-            ? rightAimPosition.position
-            : leftAimPosition.position;
-
-        Vector3 positionToLook = mainCamera.ScreenToWorldPoint(new Vector3(Mouse.current.position.ReadValue().x,
-            Mouse.current.position.ReadValue().y, 1));
-
-        direction = SpineAim.manette ? (currentDirection - PlayerController2D._instance.transform.position).normalized : (positionToLook - PlayerController2D._instance.transform.position).normalized;
+        
+        if (mainCamera != null)
+        {
+            Vector3 positionToLook =  mainCamera.ScreenToWorldPoint(new Vector3(Mouse.current.position.ReadValue().x,
+                Mouse.current.position.ReadValue().y, 1));
+            
+            direction = SpineAim.manette ? InputReader.instance.manetteDirection : (positionToLook - PlayerController2D._instance.transform.position).normalized;
+        }
     }
 }
