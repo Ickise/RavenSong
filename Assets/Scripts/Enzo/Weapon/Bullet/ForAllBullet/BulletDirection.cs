a16bdedb8ea9015f05b3475ab3ba2dd91143a168
Ickise <enzo.dufour05@gmail.com>
2024-04-04 14:25:46 +0200

    Il faut tester le tir lorsqu'on ne touche pas à la manette

--- Assets/Scripts/Enzo/Weapon/Bullet/ForAllBullet/BulletDirection.cs
+++ Assets/Scripts/Enzo/Weapon/Bullet/ForAllBullet/BulletDirection.cs
@@ -8,8 +8,6 @@ public class BulletDirection : MonoBehaviour
     [SerializeField, Header("PositionToLook.z"), Range(0.1f, 20f),
      Tooltip("Mettre la même valeur que la caméra distance dans la section body de la caméra")]
     private float cameraDistance = 10f;
-    
-    private Vector3 previousManetteDirection;
 
     private void Awake()
     {
@@ -19,26 +17,15 @@ public class BulletDirection : MonoBehaviour
     private void Init()
     {
         Camera mainCamera = Camera.main;
-        
+
         if (mainCamera != null)
         {
             Vector3 positionToLook = mainCamera.ScreenToWorldPoint(new Vector3(Mouse.current.position.ReadValue().x,
                 Mouse.current.position.ReadValue().y, cameraDistance));
-            
-            direction = SpineAim.manette
-                ? InputReader.instance.manetteDirection
-                : (positionToLook - FireOneBullet.instance.ShootPosition.position).normalized;
 
-            previousManetteDirection = InputReader.instance.manetteDirection;
-            
-            /*if (InputReader.instance.manetteDirection != Vector3.zero)
-            {
-               
-            }
-            else
-            {
-                direction = previousManetteDirection;
-            }*/
+            direction = SpineAim.manette
+                ? (FireOneBullet.instance.AimPosition.position - FireOneBullet.instance.ShootPosition.position ).normalized
+                : (positionToLook - FireOneBullet.instance.ShootPosition.position).normalized;
         }
     }
 }
