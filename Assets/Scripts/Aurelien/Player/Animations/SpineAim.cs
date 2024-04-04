79fc8599f2c919b61153ce78b9e189a81a4397af
Neroxgd <darkaux69@gmail.com>
2024-04-04 14:50:09 +0200

    aim line

--- Assets/Scripts/Aurelien/Player/Animations/SpineAim.cs
+++ Assets/Scripts/Aurelien/Player/Animations/SpineAim.cs
@@ -23,6 +23,7 @@ public class SpineAim : MonoBehaviour
         _playerAnimation = GetComponent<PlayerAnimation>();
         cam = Camera.main;
         skeletonAnimation = GetComponentsInChildren<SkeletonAnimation>();
+        aimLine.SetActive(true);
     }
 
     public void Start()
@@ -41,6 +42,8 @@ public class SpineAim : MonoBehaviour
                 aimLine.SetActive(false);
             return;
         }
+        if (!aimLine.activeInHierarchy)
+            aimLine.SetActive(true);
 
         //obligé de set individuellement les bone car le bone aim du coté gauche a le x inversé pour des raisons obscure
         if (manette && InputReader.instance.manetteDirection != Vector3.zero)
@@ -51,6 +54,9 @@ public class SpineAim : MonoBehaviour
             localPosDroite = skeletonAnimation[0].transform.InverseTransformPoint(cam.ScreenToWorldPoint(new Vector3(Mouse.current.position.ReadValue().x, Mouse.current.position.ReadValue().y, -cam.transform.position.z)));
             localPosGauche = skeletonAnimation[1].transform.InverseTransformPoint(cam.ScreenToWorldPoint(new Vector3(Mouse.current.position.ReadValue().x, Mouse.current.position.ReadValue().y, -cam.transform.position.z)));
         }
+        Vector2 currentArmePos = skeletonAnimation[_playerAnimation.GetDirection ? 0 : 1].skeleton.FindBone("Arme").GetWorldPosition(skeletonAnimation[0].transform);
+        aimLine.transform.position = currentArmePos;
+        aimLine.transform.rotation = Aurinaxtailer.Rotation2D.LookToDirection2D(aimLine.transform.rotation, _playerAnimation.GetDirection ? localPosDroite : localPosGauche);
         boneAim[0].SetLocalPosition(new Vector3(Mathf.Clamp(localPosDroite.x, 1, Mathf.Infinity), localPosDroite.y, localPosDroite.z));
         boneAim[1].SetLocalPosition(new Vector3(-Mathf.Clamp(localPosGauche.x, Mathf.NegativeInfinity, -1), localPosGauche.y, localPosGauche.z));
     }
