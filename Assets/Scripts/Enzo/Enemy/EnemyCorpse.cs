72b165af55c43fdb2fc63c597baac16af11e5a60
Ickise <enzo.dufour05@gmail.com>
2024-05-02 17:04:03 +0200

    Correction des cadavres
    Il ne tombait pas exactement au sol, désormais, il se place correctement

--- Assets/Scripts/Enzo/Enemy/EnemyCorpse.cs
+++ Assets/Scripts/Enzo/Enemy/EnemyCorpse.cs
@@ -1,15 +1,17 @@
+using System;
 using System.Collections;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 public class EnemyCorpse : MonoBehaviour
 {
-    private OnBulletHit _onBulletHit;
-
     private Collider2D corpseCollider;
 
     private Rigidbody2D corpseRigidbody;
 
     public static bool bulletInCorpse;
+
+    [SerializeField, Header("Time to destroy components"), Range(0.1f, 0.5f)] private float timeToDestroyComponents = 0.3f;
 
     private void Awake()
     {
@@ -18,20 +20,13 @@ public class EnemyCorpse : MonoBehaviour
         bulletInCorpse = true;
     }
 
-    private void Start()
-    {
-        _onBulletHit.onBulletHit.AddListener(OnBulletHit);
-    }
-
-    private void OnBulletHit(GameObject bullet)
+    private void Update()
     {
         StartCoroutine(WaitToDestroyComponents());
     }
 
     private void GetComponents()
     {
-        _onBulletHit = GetComponent<OnBulletHit>();
-
         corpseCollider = GetComponent<Collider2D>();
 
         corpseRigidbody = GetComponent<Rigidbody2D>();
@@ -39,7 +34,7 @@ public class EnemyCorpse : MonoBehaviour
 
     private void DestroyComponents()
     {
-        Object[] listToDestroy = { corpseCollider, corpseRigidbody, _onBulletHit, this };
+        Object[] listToDestroy = { corpseCollider, corpseRigidbody, this };
 
         foreach (var elementToDestroy in listToDestroy)
         {
@@ -49,13 +44,13 @@ public class EnemyCorpse : MonoBehaviour
 
     private void ChangeLayer()
     {
-        gameObject.layer = LayerMask.NameToLayer("Default");
+        gameObject.layer = LayerMask.NameToLayer("Ignore Raycast");
     }
 
     private IEnumerator WaitToDestroyComponents()
     {
         ChangeLayer();
-        yield return new WaitForSeconds(0.1f);
+        yield return new WaitForSeconds(timeToDestroyComponents);
         DestroyComponents();
         StopCoroutine(WaitToDestroyComponents());
     }
