7c49ac61428bbfef28bdebd567a5322b5b10f6b1
Ickise <enzo.dufour05@gmail.com>
2024-05-14 15:30:32 +0200

    Modification Checkpoint
    Désormais, lorsque nous sommes proches du checkpoint, il y a la touche sur laquelle il faut appuyer qui apparaît et disparaît

--- Assets/Scripts/Aurelien/Player/Checkpoint.cs
+++ Assets/Scripts/Aurelien/Player/Checkpoint.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class Checkpoint : MonoBehaviour
 {
     private static bool canActiveCheckpoint;
+    
     private static Checkpoint currentPos;
+    
     [SerializeField] private float timeActiveCheckpoint = 2;
+
+    private Image interactionSprite;
+
+    private void Awake()
+    {
+        interactionSprite = GetComponentInChildren<Image>();
+        interactionSprite.enabled = false;
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
+            interactionSprite.enabled = true;
             canActiveCheckpoint = true;
+            
             currentPos = GetComponent<Checkpoint>();
         }
     }
@@ -20,7 +33,10 @@ public class Checkpoint : MonoBehaviour
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
+        {
+            interactionSprite.enabled = false;
             canActiveCheckpoint = false;
+        }
     }
 
     public void ActiveCheckpoint(InputAction.CallbackContext context)
