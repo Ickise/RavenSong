55df21dd1fe5136d739c38b42da5f7cce6f38b33
Ickise <enzo.dufour05@gmail.com>
2024-05-27 11:36:25 +0200

    Fonctionnalité et base du menu pause

--- Assets/Scripts/Aurelien/Menu/GraphicsParameter.cs
+++ Assets/Scripts/Aurelien/Menu/GraphicsParameter.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using UnityEngine.InputSystem;
-using Unity.VisualScripting;
 
 public class GraphicsParameter : MonoBehaviour
 {
