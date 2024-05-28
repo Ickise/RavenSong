ba0683cfdf8437f43f6c77e37eb18dc36657d6dc
cyrilkordupel <kordupelcyril@gmail.com>
2024-05-28 15:55:32 +0200

    Graph Test + UI

---
+++ Assets/Scripts/Cyril/Tp/TP.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class TP : MonoBehaviour
+{
+    public GameObject _player;
+    public GameObject _pointTP;
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == _player.tag)
+        {
+            _player.gameObject.transform.position = _pointTP.transform.position;
+        }
+    }
+}
