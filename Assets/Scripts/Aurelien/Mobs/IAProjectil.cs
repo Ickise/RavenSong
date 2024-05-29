6ec919e8d37d6457551d6036a61c447c0b73f334
Neroxgd <darkaux69@gmail.com>
2024-05-29 15:55:00 +0200

    Revert "son tireur"
    This reverts commit b6b7719db3c8da2575fd7f0078083d706fa649ab.

--- Assets/Scripts/Aurelien/Mobs/IAProjectil.cs
+++ Assets/Scripts/Aurelien/Mobs/IAProjectil.cs
@@ -5,9 +5,8 @@ public class IAProjectil : IA
 {
     [SerializeField] private float minDistanceToAttack = 5f, maxDistanceToAttack = 20f, timeBetweenAttack = 3f;
     private float minPauseTime = 3f, maxPauseTime = 6f, minTimeBetweenPause = 6f, maxTimeBetweenPause = 12f;
-    [SerializeField, Header("VFX")] private GameObject projectil, VFXTir;
+    [SerializeField] private GameObject projectil, VFXTir;
     [SerializeField] private Transform posVFXTir;
-    [SerializeField, Header("Sounds")] private SoundData mort, rire, detection, idle;
     private RaycastHit2D RaycastDetectPlayerProjectil
     {
         get
@@ -57,7 +56,6 @@ public class IAProjectil : IA
         if (DetectPlayer || DetectPlayerYProjectil)
         {
             state = State.RushDistancePlayer;
-            AudioManager.instance.PlaySound(detection);
             currentSpeedMovement = speedAttaquePlayer;
         }
     }
@@ -76,7 +74,6 @@ public class IAProjectil : IA
             if (!DetectPlayer && !DetectPlayerYProjectil)
             {
                 state = State.Roaming;
-                AudioManager.instance.PlaySound(idle);
                 return;
             }
             rb2D.velocity = Vector2.zero;
@@ -98,7 +95,6 @@ public class IAProjectil : IA
             }
             StopAllCoroutines();
             state = State.Roaming;
-            AudioManager.instance.PlaySound(idle);
             return;
         }
         if (Mathf.Abs(posATK - transform.position.x) < 0.2f)
@@ -138,12 +134,6 @@ public class IAProjectil : IA
     private void OnDisable()
     {
         state = State.Roaming;
-        AudioManager.instance.PlaySound(idle);
         rb2D.constraints = RigidbodyConstraints2D.FreezePositionX;
-    }
-
-    private void OnDestroy()
-    {
-        AudioManager.instance.PlaySound(mort);
     }
 }
