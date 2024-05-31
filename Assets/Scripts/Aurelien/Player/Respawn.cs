c08ca2777aa68a8b941e3271aad814566ae6ee64
Ickise <enzo.dufour05@gmail.com>
2024-05-31 17:12:58 +0200

    Le changement de light reste même après la mort

--- Assets/Scripts/Aurelien/Player/Respawn.cs
+++ Assets/Scripts/Aurelien/Player/Respawn.cs
@@ -27,6 +27,8 @@ public class Respawn : MonoBehaviour
 
     private bool spawn, death;
 
+    public static bool alreadyDeath;
+
     public static Vector2 spawnPosition;
     public static bool checkPoint;
 
@@ -95,6 +97,7 @@ public class Respawn : MonoBehaviour
 
     private void Death()
     {
+        alreadyDeath = true;
         ControlsParameter.GamePadVibration(this, 0.4f, 0.4f, 0.8f);
         playerInput.enabled = false;
         shaderDeathRespawn =
