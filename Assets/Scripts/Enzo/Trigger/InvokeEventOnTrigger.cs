c658820aec652518aab6b18305705cf530a53462
Ickise <enzo.dufour05@gmail.com>
2024-06-01 18:03:07 +0200

    Préfab et show name place

--- Assets/Scripts/Enzo/Trigger/InvokeEventOnTrigger.cs
+++ Assets/Scripts/Enzo/Trigger/InvokeEventOnTrigger.cs
@@ -8,7 +8,7 @@ public class InvokeEventOnTrigger : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
-        {
+        {   
             onTriggerEvent.Invoke();
         }
     }
