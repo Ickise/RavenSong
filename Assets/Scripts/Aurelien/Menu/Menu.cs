4758af3caf8ac8d47c35d9f24de7756cd1cc843e
Neroxgd <darkaux69@gmail.com>
2024-06-01 16:06:54 +0200

    tout les sons du menu sauf le livre

--- Assets/Scripts/Aurelien/Menu/Menu.cs
+++ Assets/Scripts/Aurelien/Menu/Menu.cs
@@ -1,11 +1,12 @@
 using UnityEngine.SceneManagement;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using DG.Tweening;
 
 public class Menu : MonoBehaviour
 {
     [SerializeField] private GameObject firstButtonSelected;
-
+    [SerializeField] private SoundData quitGame;
     [SerializeField] private string sceneNameToPlay;
 
     private void Start()
@@ -21,6 +22,9 @@ public class Menu : MonoBehaviour
 
     public void Quit()
     {
+        AudioManager.instance.PlaySound(quitGame);
+        float a = 0;
+        DOTween.To(() => a, x => a = x, 1, 0.5f);
         Application.Quit();
     }
 
--- Assets/Scripts/Enzo/Input/MenuInputReader.cs
+++ Assets/Scripts/Enzo/Input/MenuInputReader.cs
@@ -4,13 +4,21 @@ using UnityEngine.InputSystem;
 public class MenuInputReader : MonoBehaviour
 {
     private BackInMenu activeBackInMenu;
+    [SerializeField] private SoundData back, selection;
 
     public void OnBack(InputAction.CallbackContext context)
     {
         if (context.started)
         {
+            AudioManager.instance.PlaySound(back);
             activeBackInMenu = gameObject.GetComponentInChildren<BackInMenu>();
             activeBackInMenu.Back();
         }
+    }
+
+    public void VerticalNavigation(InputAction.CallbackContext context)
+    {
+        if (!context.started) return;
+        AudioManager.instance.PlaySound(selection);
     }
 }
