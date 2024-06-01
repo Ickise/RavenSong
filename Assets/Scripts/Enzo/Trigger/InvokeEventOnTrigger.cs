80d72bcdb078c3e60d2b7302ce5cf733daf73bf2
Ickise <enzo.dufour05@gmail.com>
2024-06-01 17:33:51 +0200

    beaucoup de choses

---
+++ Assets/Scripts/Enzo/Trigger/InvokeEventOnTrigger.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InvokeEventOnTrigger : MonoBehaviour
+{
+    public UnityEvent onTriggerEvent = new UnityEvent();
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            onTriggerEvent.Invoke();
+        }
+    }
+}
