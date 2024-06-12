daf18d440c006cf8c434845f244511337f7487cc
Ickise <enzo.dufour05@gmail.com>
2024-06-12 11:34:01 +0200

    J'ai remis les particules du checkpoint en rouge

--- Assets/Scripts/Cyril/Tp/TP.cs
+++ Assets/Scripts/Cyril/Tp/TP.cs
@@ -7,9 +7,9 @@ public class TP : MonoBehaviour
     
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == _player.tag)
+        if(collision.CompareTag("Player"))
         {
-            _player.gameObject.transform.position = _pointTP.transform.position;
+            _player.transform.position = _pointTP.transform.position;
         }
     }
 }
